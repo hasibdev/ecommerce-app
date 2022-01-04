42edb12bc74b5b2a797fd88602b6466ad521328d
hasibdev <79460308+hasibdev@users.noreply.github.com>
2022-01-04 18:40:37 +0600

    design updated

--- src/components/home/TodaysBest.vue
+++ src/components/home/TodaysBest.vue
@@ -1,14 +1,16 @@
 <template>
    <div>
-      <h5 class="q-mb-lg">Today's Best Deal</h5>
+      <div class="q-mb-lg flex justify-between">
+         <h5>Today's Best Deal</h5>
+         <span>View All</span>
+      </div>
 
-      <div class="row">
-         <div class="col">
+      <div class="row q-col-gutter-md">
+         <div class="col-6">
             <q-card class="shadow-6">
                <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
-
                <q-card-section>
-                  <q-btn fab color="primary" icon="place" class="absolute" style="top: 0; right: 12px; transform: translateY(-50%);" />
+                  <!-- <q-btn fab color="primary" icon="shopping_cart" class="absolute" style="top: 0; right: 12px; transform: translateY(-50%);" /> -->
 
                   <div class="row no-wrap items-center">
                      <div class="col text-h6 ellipsis">
@@ -20,7 +22,27 @@
 
                   <p class="text-red-12 text-h5 text-bold q-mt-md">$32.50</p>
 
-                  <q-btn outline color="orange-7" icon="las la-cart-arrow-down" label="ADD TO CART" class="full-width q-mt-lg q-py-sm" />
+                  <!-- <q-btn outline color="orange-7" icon="las la-cart-arrow-down" label="ADD TO CART" class="full-width q-mt-lg q-py-sm" /> -->
+               </q-card-section>
+            </q-card>
+         </div>
+         <div class="col-6">
+            <q-card class="shadow-6">
+               <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+               <q-card-section>
+                  <!-- <q-btn fab color="primary" icon="shopping_cart" class="absolute" style="top: 0; right: 12px; transform: translateY(-50%);" /> -->
+
+                  <div class="row no-wrap items-center">
+                     <div class="col text-h6 ellipsis">
+                        Cafe Basilico
+                     </div>
+                  </div>
+
+                  <q-rating v-model="stars" :max="5" size="20px" />
+
+                  <p class="text-red-12 text-h5 text-bold q-mt-md">$32.50</p>
+
+                  <!-- <q-btn outline color="orange-7" icon="las la-cart-arrow-down" label="ADD TO CART" class="full-width q-mt-lg q-py-sm" /> -->
                </q-card-section>
             </q-card>
          </div>
