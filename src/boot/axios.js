5577063c079dc0397765b6e398b0b5ca76848530
hasibdev <79460308+hasibdev@users.noreply.github.com>
2022-01-05 19:01:40 +0600

    fix process

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-const api = axios.create({ baseURL: process.env.API_URL })
+const api = axios.create({ baseURL: 'https://phplaravel-692081-2338552.cloudwaysapps.com/api' })
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios
